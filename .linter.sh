c19eadc1f7571493efde4bc04bbc6f859c0dd626
Kavia AI <production@kavia.ai>
2025-06-07 13:02:22 +0000

    CheckPoint

--- .linter.sh
+++ .linter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/kavia/workspace/code-generation/synthwave-space-shooter-28084-a84576a4/synthwave_space_shooter
+cd /home/kavia/workspace/code-generation/copy-of-synthwave-space-shooter-28084-synthwave-space-shooter-28084-a84576a4-35561/synthwave_space_shooter
 npm run build
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
